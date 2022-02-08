baecc21754ed0c457ce4e2834a24a1fa2991c68c
MauroV27 <51689397+MauroV27@users.noreply.github.com>
2022-02-08 02:22:44 -0300

    This directory has been renamed
    src >> entities

--- src/entities/Asteroid/AsteroidType.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-class_name AsteroidType
-
-export var life : int = 1
-export var scores : int = 10
-export var size : int = 1
-export var color : Color = Color("#ebbd1a")
-export var speed_increase : float = 1.0
